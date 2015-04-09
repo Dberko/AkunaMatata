5dd45430a137338323cd99f741c1dd2445a3f86f
Daniel Berkowitz <danberko@umich.edu>
2015-04-09 10:52:03 -0400

    board complete

--- connect.py
+++ connect.py
@@ -40,21 +40,23 @@ class player:
 
         return col;
 
-    ## NOTE(DANIEL): Don't think this is right
+    def alphabeta():
+        col = 0
+        return col
+
     def getColCount(self, fakeBoard, col):
         count = 0
-        for col in range(7):
-            for row in range(6):
-                if (fakeBoard[col][row] in [1,2]):
-                    count += 1
+        for row in range(6):
+            if (fakeBoard[col][row] in [1,2]):
+                count += 1
         return count
 
     def isValidMove(self, fakeBoard, col):
-        return self.getColCount(fakeBoard, col) < 7 
+        return self.getColCount(fakeBoard, col) < 6 
 
     def makeMove(self, fakeBoard, col, me):
         colcount = self.getColCount(fakeBoard, col)
-        if (count < height):
+        if (self.isValidMove(fakeBoard, col)):
             if (me):
                 sign = self.playernum
             else:
@@ -83,19 +85,80 @@ class player:
                 return True
         return False
 
+    def hasWinner(self, fakeBoard):
+        return self.getWinner(fakeBoard) != 0
 
-def main():
-    # 7 x 6
-    p = player("Dan", 1)
-    fboard = [[1,1,1,1,2,2],
-              [0,0,0,0,0,0],
-              [0,0,0,0,0,0],     ## This is North for board
-              [0,0,0,0,0,0],
-              [0,0,0,0,0,0],
-              [0,0,0,0,0,0],
-              [0,0,0,0,0,0]]
-    printBoard(fboard)
-    print(p.isValidMove(fboard, 0))
+    def getWinner(self, fakeBoard):
+        for col in range(7):
+            for row in range(6 - 3):
+                playerWin = True
+                aiWin = True
+                for o in range(4):
+                    if (playerWin and fakeBoard[col][row + o] != self.playernum):
+                        playerWin = False
+                    if (self.playernum == 1):
+                        aiNum = 2
+                    elif (self.playernum == 2):
+                        aiNum = 1
+                    if (aiWin and fakeBoard[col][row + o] != aiNum):
+                        aiWin = False
+                if (playerWin):
+                    return self.playernum
+                elif (aiWin):
+                    return aiNum
+
+        for row in range(6 - 3):
+            for col in range(7 - 3):
+                playerWin = True
+                aiWin = True
+                for o in range(4):
+                    if (playerWin and fakeBoard[col + o][row] != self.playernum):
+                        playerWin = False
+                    if (self.playernum == 1):
+                        aiNum = 2
+                    elif (self.playernum == 2):
+                        aiNum = 1
+                    if (aiWin and fakeBoard[col + o][row] != aiNum):
+                        aiWin = False
+                if (playerWin):
+                    return self.playernum
+                elif (aiWin):
+                    return aiNum
+
+        for col in range(6, 2, -1):
+            for row in range(6 - 3):
+                playerWin = True
+                aiWin = True
+                for o in range(4):
+                    if (playerWin and board[col - o][row + o] != self.playernum):
+                        playerWin = False
+                    if (self.playernum == 1):
+                        aiNum = 2
+                    elif (self.playernum == 2):
+                        aiNum = 1
+                    if (aiWin and board[col - o][row + o] != aiNum):
+                        aiWin = False
+                if (playerWin):
+                    return self.playernum
+                elif(aiWin):
+                    return aiWin
+        return 0
+
+    def playerIsWinner(self):
+        return self.getWinner() == self.playernum
+
+    def isFull(self, fakeBoard):
+        emptyCol = False
+        for col in range(7):
+            if (self.getColCount(fakeBoard, col) < 6):
+                emptyCol = True
+                break
+        return not emptyCol
+
+    def isTie(self):
+        return self.isFull(self, fakeBoard) and self.getWinner() == 0
+
+
 
 def printBoard(board):
     for row in range(-1, -7, -1):
@@ -105,6 +168,22 @@ def printBoard(board):
            print(board[col][row], end='')
 
         print()
+
+
+def main():
+    # 7 x 6
+    p = player("Dan", 1)
+    fboard = [[1,1,2,2,2,2],
+              [1,1,1,1,1,1],
+              [1,1,1,1,1,1],     ## This is North for board
+              [1,1,1,1,1,1],
+              [1,1,1,1,1,1],
+              [1,1,1,1,1,1],
+              [1,1,1,1,1,1]]
+
+    printBoard(fboard)
+    winner = p.getWinner(fboard);
+    print(p.isFull(fboard))
 
 if __name__ == "__main__":
     main()
